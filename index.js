bf1c86690e0c7fe895c0b73497f236eba98a475b
hybridx <hybridx18@gmail.com>
2021-05-11 23:41:32 +0530

    chore(#6) - Changed the bot to support basic commands

--- index.js
+++ index.js
@@ -1,24 +1,82 @@
-import express from 'express'
-import { ApolloServer } from 'apollo-server-express'
-import { resolvers } from './src/resolvers'
-import { typeDefs } from './src/typeDefs'
-import dotEnv from 'dotenv';
+const express = require('express');
+const JiraApi = require ('jira-client');
+require('dotenv').config();
 
-dotEnv.config()
-
-const PORT = process.env.PORT || 3500
-const app = express()
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    playground: true,
+const PORT = process.env.PORT || 9000;
+const app = express().use(express.urlencoded({extended: false})).use(express.json());
+const jira = new JiraApi({
+  protocol: 'https',
+  host: process.env.JIRA_HOST,
+  username: process.env.JIRA_USER,
+  password: process.env.JIRA_PASS,
+  apiVersion: '2',
+  strictSSL: false
 })
 
-server.applyMiddleware({ app })
+app.listen(PORT, () => { console.info(`Listening on Port# - ${PORT}`); })
 
 app.get('/', function(req, res) {
   res.send({ hello: 'there!' })
 })
 
-app.listen(PORT, () => `Listening at http://localhost:${PORT}`)
+app.post('/', async (req, res) => {
+  let message = '';
+  // console.info(req.body);
+  if (req.body && req.body.type) {
+    switch(req.body.type) {
+      case 'ADDED_TO_SPACE':
+        message = `Thank you for adding me. Command \help`;
+        break;
+      case 'MESSAGE':
+        // message = await fetchJIRAInfo(textParser(req.body.message.argumentText));
+        message = await textParser(req.body.message.argumentText);
+        break;
+      default:
+        message = 'Something went wrong. Please contact One Platform for any questions';
+    }
+  }
+  return res.json({ 'text': JSON.stringify(message) });
+});
+
+// Start JBot functions
+/**
+ * Parses the text message according to what the text message is and responds accordingly
+ * @param {text message from user} argumentText 
+ * @returns Sanitized text which also sends the appropriate response
+ */
+async function textParser(argumentText) {
+  const regex = /\b\w*[-']\w*\b/g;
+  const issueTitles = argumentText
+    .match(regex)
+    .map(word => word.toUpperCase());
+  const issues = await Promise.all(issueTitles.map(issue => fetchJIRAInfo(issue)));
+  return issues;
+}
+
+/**
+ * Fetches JIRA information from the JIRA server
+ * @param {JIRA Issue number} issue 
+ * @returns Information about the JIRA - summary, status, reporter, assignee and priority
+ */
+async function fetchJIRAInfo(issue) {
+  let msg = '';
+  try {
+    const jiraDetails = await jira.getIssue(issue).then(res => res);
+    if (jiraDetails.statusCode === 404) {
+      throw 'JIRA not found';
+    }
+    msg = {
+      summary: `${jiraDetails.fields.summary}`,
+      status: `${jiraDetails.fields.status.name}`,
+      reporter: `${jiraDetails.fields.reporter.name}`,
+      assignee: `${jiraDetails.fields.assignee.name}`,
+      priority: `${jiraDetails.fields.priority.name}`,
+      url: `https://projects.engineering.redhat.com/browse/${jiraDetails.key}`,
+    };
+    return msg;
+  } catch (error) {
+    // console.error(error);
+    return `Something went wrong - ${error}`;
+  }
+}
+
