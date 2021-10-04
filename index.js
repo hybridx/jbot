f93acdc44cd5888489b863d3fc970386f7ab20c2
Gautam krishna R <rgautamkrishna@gmail.com>
2021-10-04 16:28:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const jira = new JiraApi({
 
 app.listen(PORT, () => { console.info(`Listening on Port# - ${PORT}`); })
 
-app.get('/', function(res, statusCode, data) {
-  res.writeHead(statusCode, {'Content-Type': 'text/html'});
+app.get('/', function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(`<h1>hello world</h1>`);
   res.end();
 })
